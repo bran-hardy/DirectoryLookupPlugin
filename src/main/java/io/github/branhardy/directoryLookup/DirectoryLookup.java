901768b65ed4474565334e8b6f182741b123b263
Brandon Hardy <90705067+bran-hardy@users.noreply.github.com>
2024-10-21 12:32:47 -0400

    Reworked filtering system to be easier to update

--- src/main/java/io/github/branhardy/directoryLookup/DirectoryLookup.java
+++ src/main/java/io/github/branhardy/directoryLookup/DirectoryLookup.java
@@ -6,6 +6,8 @@ import io.github.branhardy.directoryLookup.services.NotionService;
 import io.github.branhardy.directoryLookup.utils.FilterUtil;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Map;
+import java.util.Objects;
 import java.util.logging.Logger;
 
 public final class DirectoryLookup extends JavaPlugin {
@@ -32,8 +34,6 @@ public final class DirectoryLookup extends JavaPlugin {
 
         this.getCommand("shop").setExecutor(new ShopCommand(notionService, shopDatabase));
         this.getCommand("shop").setTabCompleter(new ShopTabCompleter());
-
-        FilterUtil.initialize();
     }
 
     @Override
--- src/main/java/io/github/branhardy/directoryLookup/utils/FilterUtil.java
+++ src/main/java/io/github/branhardy/directoryLookup/utils/FilterUtil.java
@@ -3,87 +3,12 @@ package io.github.branhardy.directoryLookup.utils;
 import io.github.branhardy.directoryLookup.DirectoryLookup;
 import org.bukkit.Material;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class FilterUtil {
 
-    public static List<String> pottery_sherd_names;
-    public static List<String> armor_trim_template_names;
-
-    public static List<String> diamond_tools, diamond_armor;
-    public static List<String> horse_armor;
-
-    public static List<String> flowers, dyes;
-
-    public static void initialize() {
-        pottery_sherd_names = getItemsWithSuffix("_SHERD");
-        armor_trim_template_names = getItemsWithSuffix("_TRIM_SMITHING_TEMPLATE");
-
-        diamond_tools = Arrays.asList(
-                "diamond_pickaxe",
-                "diamond_axe",
-                "diamond_shovel",
-                "diamond_hoe",
-                "diamond_sword"
-        );
-
-        diamond_armor = Arrays.asList(
-                "diamond_helmet",
-                "diamond_chestplate",
-                "diamond_leggings",
-                "diamond_boots"
-        );
-
-        horse_armor = Arrays.asList(
-                "leather_horse_armor",
-                "iron_horse_armor",
-                "golden_horse_armor",
-                "diamond_horse_armor"
-        );
-
-        flowers = Arrays.asList(
-                "allium",
-                "azure_bluet",
-                "blue_orchid",
-                "cornflower",
-                "dandelion",
-                "lily_of_the_valley",
-                "oxeye_daisy",
-                "poppy",
-                "torch_flower",
-                "orange_tulip",
-                "pink_tulip",
-                "red_tulip",
-                "white_tulip",
-                "wither_rose",
-                "lilac",
-                "peony",
-                "pitcher_plant",
-                "rose_bush",
-                "sunflower"
-        );
-
-        dyes = getItemsContaining("_DYE");
-
-        DirectoryLookup.logger.info("Filters have been initialized");
-    }
-
-    public static List<String> getItemsWithSuffix(String matchingSuffix) {
-        return Arrays.stream(Material.values())
-                .map(Material::name)
-                .filter(name -> name.endsWith(matchingSuffix))
-                .collect(Collectors.toList());
-    }
-
-    public static List<String> getItemsContaining(String matchingName) {
-        return Arrays.stream(Material.values())
-                .map(Material::name)
-                .filter(name -> name.contains(matchingName))
-                .collect(Collectors.toList());
-    }
+    private static Map<String, Object> filters;
 
     // This will add additional filters for diamond tools and armor (i.e. if someone searches for diamond_axe,
     // diamond_tools will be added to the filter)
@@ -93,19 +18,36 @@ public class FilterUtil {
     public static List<String> setupNotionFilter(String initialFilter) {
         List<String> filterList = new ArrayList<>();
 
+        filters = DirectoryLookup.instance.getConfig().getConfigurationSection("filters").getValues(false);
+
         filterList.add(reformatFilter(initialFilter));
 
-        if (pottery_sherd_names.contains(initialFilter)) filterList.add("Sherds");
-        if (armor_trim_template_names.contains(initialFilter)) filterList.add(" Armor Trims");
-        if (diamond_tools.contains(initialFilter)) filterList.add("Diamond Tools");
-        if (diamond_armor.contains(initialFilter)) filterList.add("Diamond Armor");
-        if (horse_armor.contains(initialFilter)) filterList.add("Horse Armor");
-        if (flowers.contains((initialFilter))) filterList.add("Flowers");
-        if (dyes.contains((initialFilter))) filterList.add("Dye");
+        for (Map.Entry<String, Object> entry : filters.entrySet()) {
+            String filterName = entry.getKey();
+            List<String> items = castToStringList(entry.getValue());
+
+            // If a filter group can be filtered using the Material values, it will start with _
+            if (items.getFirst().startsWith("_")){
+                items = getItemsWithSuffix(items.getFirst().toLowerCase(Locale.ROOT));
+            }
+
+            if (items.contains(initialFilter)) {
+                filterList.add(reformatFilter(filterName));
+            }
+
+            DirectoryLookup.logger.info(reformatFilter(filterName) + ": " + items);
+        }
 
         DirectoryLookup.logger.info("Filter has been created: " + filterList);
 
         return filterList;
+    }
+
+    public static List<String> getItemsWithSuffix(String matchingSuffix) {
+        return Arrays.stream(Material.values())
+                .map(material -> material.name().toLowerCase(Locale.ROOT))
+                .filter(name -> name.endsWith(matchingSuffix))
+                .collect(Collectors.toList());
     }
 
     // Reformat the input filter so that it can properly query the notion database
@@ -120,5 +62,16 @@ public class FilterUtil {
         }
 
         return outputString.toString();
+    }
+
+    private static List<String> castToStringList(Object obj) {
+        if (obj instanceof List<?>) {
+            return ((List<?>) obj).stream()
+                    .filter(element -> element instanceof String)
+                    .map(element -> (String) element)
+                    .collect(Collectors.toList());
+        }
+
+        return null;
     }
 }
